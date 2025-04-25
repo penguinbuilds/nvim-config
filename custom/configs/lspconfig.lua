060b8f6fa87da3d79153aea5195cfd84e79ae94f
Shaaswat <shaaswatkjha@gmail.com>
2025-04-25 18:18:53 +0530

    first commit

---
+++ custom/configs/lspconfig.lua
@@ -0,0 +1,27 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+
+local servers = {
+  "pyright",
+  "ruff",
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"python"},
+  })
+end
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
+
